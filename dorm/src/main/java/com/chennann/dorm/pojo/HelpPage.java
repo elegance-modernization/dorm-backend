9e30cc612e91d3cc77f981b15b0838d7582032da
yizhilsy <1937714332@qq.com>
2024-01-04 00:51:36 +0800

    devlsy init

---
+++ dorm/src/main/java/com/chennann/dorm/pojo/HelpPage.java
@@ -0,0 +1,23 @@
+package com.chennann.dorm.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelpPage {
+    private Integer id;
+    private String username;
+    private String name;
+    private String phone;
+    private String title;
+    private String content;
+    private String image;
+    private Short typeId;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+}
